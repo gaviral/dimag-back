468d4b9f645c1ede68838d94c0b61dac04f36952
Aviral Garg <aviral.garg@icloud.com>
2023-12-28 21:27:47 -0800

    Refactor server.js file to enable CORS

--- api/server.js
+++ api/server.js
@@ -1,31 +1,32 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-
-// Uncomment to allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('db.json')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join('db.json');
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db)
+const router = jsonServer.router(db);
 
 // Comment out to allow write operations
 // const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-// Add this before server.use(router). It's pur
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/blog/:resource/:id/show': '/:resource/:id'
-// }))
-server.use(router)
+// Enable CORS by setting appropriate headers
+server.use((req, res, next) => {
+  // Replace '*' with the actual origin(s) you want to allow
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
 server.listen(3000, () => {
     console.log('JSON Server is running')
-})
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
