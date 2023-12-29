1f6729df8bdd627883bca69a9ffe9bab44928953
Aviral Garg <aviral.garg@icloud.com>
2023-12-28 20:48:45 -0800

    Refactor JSON Server setup in server.js

--- api/server.js
+++ api/server.js
@@ -1,39 +1,31 @@
-// Import the json-server module to create a JSON API server.
-const jsonServer = require('json-server');
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-// Create an instance of a JSON Server.
-const server = jsonServer.create();
+const server = jsonServer.create()
 
-// Import fs and path modules to work with the file system.
-const fs = require('fs');
-const path = require('path');
-
-// Define the path to the db.json file.
-const filePath = path.join('db.json');
-
-// Read the contents of the db.json file.
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
-
-// Parse the JSON data from the db.json file.
 const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
-// Create a router for the JSON Server using the parsed db.json data.
-// This allows json-server to provide routes based on this data.
-const router = jsonServer.router(db);
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
-// Set default middlewares (logger, static, CORS, and no-cache) provided by json-server.
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults()
 
-// Apply the default middlewares.
-server.use(middlewares);
-
-// Mount the router on the server.
-server.use(router);
-
-// Start the server on port 3000 and log a message to the console.
+server.use(middlewares)
+// Add this before server.use(router). It's pur
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1',
+//     '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
+    console.log('JSON Server is running')
+})
 
-// Export the server object for use elsewhere, like in testing or when imported into other modules.
-module.exports = server;
+// Export the Server API
+module.exports = server
